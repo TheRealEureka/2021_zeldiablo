44ddbdc78a1ac0f91a489552fd3ba58a27a410ce
perrin446u <perrin446u@eut.univ-lorraine.fr>
2021-05-27 11:09:55 +0200

    rajout test subirDegats pour héros

--- projet_zeldiablo/src/Test/TestMonstres.java
+++ projet_zeldiablo/src/Test/TestMonstres.java
@@ -11,15 +11,37 @@ public class TestMonstres {
 		Monstre m = new Monstre();
 		boolean res = m == null;
 		assertFalse("Le monstre ne devrait pas être null", res);
-		
+
 	}
-	
+
 	@Test
 	public void testConstructeurMonstreInvalide() {
 		Monstre m;
 		boolean res = m == null;
 		assertTrue("Le monstre ne devrait pas être null", res);
-		
+
+	}
+
+	/**
+	 * Test l'action subir dégats quand c'est juste
+	 */
+	public void test_subirDegats_OK() {
+		Labyrinthe labyrinthe = new Labyrinthe();
+		heros h = new heros("Stib", 5, 2);
+		h.subirDegats(5);
+		boolean res = h.getPv() == 0;
+		assertEquals("héros subis dégats", true, res);
+	}
+
+	/**
+	 * Test l'action subir dégats quand les degats subis sont à 0
+	 */
+	public void test_subirDegats_PasOK() {
+		Labyrinthe labyrinthe = new Labyrinthe();
+		heros h = new heros("Stib", 5, 2);
+		h.subirDegats(-5);
+		boolean res = h.getPv() == 0;
+		assertEquals("héros ne subis pas de dégats", false, res);
 	}
 
 }
