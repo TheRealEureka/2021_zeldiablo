7d6f00c32f05cc37364e90d07c21e6c97af7bef6
Thomas STIBLING <thomas.stibling@etu.univ-lorraine.fr>
2021-05-27 10:59:42 +0200

    test  labyrinthe constructeur

--- projet_zeldiablo/src/Test/TestLabyrinthe.java
+++ projet_zeldiablo/src/Test/TestLabyrinthe.java
@@ -12,28 +12,34 @@ public class TestLabyrinthe {
 		assertEquals("le labyrinthe est trop petit en hauteur", 10, labi.getAuteur());
 	}
 	
+	@Test
 	public void testConstructeurLargeurFauxtp() {
 		Labyrinthe labi = new Labyrinthe(2,10);
 		assertEquals("le labyrinthe est trop petit en largeur", 10, labi.getLargeur());
 	}
 	
+	@Test
 	public void testConstructeurHauteurFauxtg() {
 		Labyrinthe labi = new Labyrinthe(10,20);
 		assertEquals("le labyrinthe est trop grand en hauteur", 10, labi.getAuteur());
 	}
 	
+	@Test
 	public void testConstructeurLargeurFauxtg() {
 		Labyrinthe labi = new Labyrinthe(20,10);
 		assertEquals("le labyrinthe est trop grand en largeur", 10, labi.getLargeur());
 	}
-
+	
+	@Test
 	public void testConstructeurHauteurVrai() {
 		Labyrinthe labi = new Labyrinthe(10,10);
 		assertEquals("le labyrinthe est à la bonne taille", 10, labi.getAuteur());
 	}
 	
+	@Test
 	public void testConstructeurLargeurVrai() {
 		Labyrinthe labi = new Labyrinthe(10,10);
 		assertEquals("le labyrinthe est à la bonne taille", 10, labi.getLargeur());
 	}
+	
 }
