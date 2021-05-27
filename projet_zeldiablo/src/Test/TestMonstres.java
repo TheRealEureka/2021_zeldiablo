a36307d775a84f9ad00ca9ecdc83754a749f338d
Thomas <Thomas@DESKTOP-UAU5GCS>
2021-05-27 10:50:23 +0200

    Ajout du test du constructeur pour le monstre

--- projet_zeldiablo/src/Test/TestMonstres.java
+++ projet_zeldiablo/src/Test/TestMonstres.java
@@ -8,10 +8,7 @@ public class TestMonstres {
 
 	@Test
 	public void testConstructeurMonstre() {
-		Jeu j = new Jeu();
-		j.genererLabyrinthe();
 		Monstre m = new Monstre();
-		j.ajouterMonstre(m);
 		assertFalse("Le monstre ne devrait pas être null", m == null);
 		
 	}
