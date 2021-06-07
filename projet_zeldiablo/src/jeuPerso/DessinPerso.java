93f6d5edbe037c916ebaa92685cefe5af7aa1b5b
Kévin BC <k.bullycimbaluria@gmail.com>
2021-06-07 20:44:03 +0200

    Ajout des textures

--- projet_zeldiablo/src/jeuPerso/DessinPerso.java
+++ projet_zeldiablo/src/jeuPerso/DessinPerso.java
@@ -3,7 +3,11 @@ package jeuPerso;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
 
 import Labyrinthe.Case;
 import Labyrinthe.Labyrinthe;
@@ -27,11 +31,11 @@ public class DessinPerso implements DessinJeu {
 	 */
 	private JeuPerso jeu;
 	private Labyrinthe laby;
+
 	/**
 	 * appelle constructeur parent
 	 * 
-	 * @param j
-	 *            le jeutest a afficher
+	 * @param j le jeutest a afficher
 	 */
 	public DessinPerso(JeuPerso j, Labyrinthe lab) {
 		this.jeu = j;
@@ -39,40 +43,64 @@ public class DessinPerso implements DessinJeu {
 	}
 
 	/**
-	 * dessiner un objet consiste a dessiner sur l'image suivante methode
-	 * redefinie de Afficheur
+	 * dessiner un objet consiste a dessiner sur l'image suivante methode redefinie
+	 * de Afficheur
+	 * 
+	 * @throws IOException
 	 */
 	private void dessinerObjet(String s, int x, int y, BufferedImage im) {
 		Graphics2D crayon = (Graphics2D) im.getGraphics();
-	
-		
+		BufferedImage image = null;
+		BufferedImage background = null;
+		BufferedImage stone = null;
+		BufferedImage cd_off = null;
+		BufferedImage cd_on = null;
+		try {
+			image = ImageIO.read(new File("src\\images\\2.png"));
+			background =  ImageIO.read(new File("src\\images\\background.png"));
+			stone =  ImageIO.read(new File("src\\images\\stone.png"));
+			cd_off =  ImageIO.read(new File("src\\images\\cd_off.png"));
+			cd_on =  ImageIO.read(new File("src\\images\\cd_on.png"));
+		} catch (IOException e) {
+		}
+
 		switch (s) {
+		case "BACKGROUND" : 
+			for(int i = 0 ; i <= 15 ; i++)
+			{
+				for(int t = 0 ; t<= 15 ;t++)
+				{
+					crayon.drawImage(background, i * TAILLE_CASE, t * TAILLE_CASE, null);
+				}
+			}
+			break;
 		case "PJ":
-			crayon.setColor(Color.blue);
-			crayon.fillOval(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE,
-					TAILLE_CASE);
+			// crayon.setColor(Color.blue);
+			// crayon.fillOval(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE);
+			crayon.drawImage(image, x * TAILLE_CASE, y * TAILLE_CASE, null);
 			break;
 		case "MUR":
-			crayon.setColor(Color.gray);
-			crayon.fillRect(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE,
-					TAILLE_CASE);
+			//crayon.setColor(Color.gray);
+			//crayon.fillRect(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE);
+			crayon.drawImage(stone, x * TAILLE_CASE, y * TAILLE_CASE, null);
 			crayon.setColor(Color.black);
-			crayon.drawRect(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE,
-					TAILLE_CASE);
+			crayon.drawRect(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE);
 			break;
 		case "DEC":
-			crayon.setColor(Color.green);
-			crayon.fillRect(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE,
-					TAILLE_CASE);
+			//crayon.setColor(Color.green);
+			//crayon.fillRect(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE);
+			crayon.drawImage(cd_on, x * TAILLE_CASE, y * TAILLE_CASE, null);
 			break;
 		case "DEC_USED":
-			crayon.setColor(Color.red);
-			crayon.fillRect(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE,
-					TAILLE_CASE);
+			//crayon.setColor(Color.red);
+			//crayon.fillRect(x * TAILLE_CASE, y * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE);
+			crayon.drawImage(cd_off, x * TAILLE_CASE, y * TAILLE_CASE, null);
 			break;
 		default:
 			throw new AssertionError("objet inexistant");
 		}
+		
+
 	}
 
 	/**
@@ -80,15 +108,13 @@ public class DessinPerso implements DessinJeu {
 	 */
 	public void dessiner(BufferedImage im) {
 		Personnage pj = jeu.getPj();
+		this.dessinerObjet("BACKGROUND",0, 0, im);
 		ArrayList<Case> tab = laby.getTab();
-		for(int i = 0 ; i < tab.size(); i++)
-		{
+		for (int i = 0; i < tab.size(); i++) {
 			this.dessinerObjet(tab.get(i).getType(), tab.get(i).getX(), tab.get(i).getY(), im);
 		}
 		this.dessinerObjet("PJ", pj.getPosX(), pj.getPosY(), im);
 
-		
-		
 	}
 
 }
