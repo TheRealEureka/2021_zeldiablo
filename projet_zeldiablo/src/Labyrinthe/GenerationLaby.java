40ea079ea918b6d1ebf323195434a23e41824e34
BullyCimbaluria Kevin <kevin.bully-cimbaluria5@etu.univ-lorraine.fr>
2021-06-07 17:27:42 +0200

    Generation Laby (2)

---
+++ projet_zeldiablo/src/Labyrinthe/GenerationLaby.java
@@ -0,0 +1,24 @@
+package Labyrinthe;
+
+public class GenerationLaby {
+	private Labyrinthe l;
+	public GenerationLaby(Labyrinthe l)
+	{
+		this.l=l;
+	}
+	
+	public void labyTest()
+	{
+		for(int i =0 ; i < 8 ; i++)
+		{
+			l.addCase(new Case(1,i));
+			l.addCase(new Case(i,9));
+
+		}
+		for(int i =1 ; i < 8 ; i++)
+		{
+			l.addCase(new Case(i,7));		
+		}
+		
+	}
+}
