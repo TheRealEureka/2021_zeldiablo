01c2e8db0cc7873fe2fee3dc5eaa183274eae808
daprile3u <thomas.d-aprile6@etu.univ-lorraine.fr>
2021-06-07 11:37:36 +0200

    Modifications

--- projet_zeldiablo/src/Labyrinthe/Jeu.java
+++
@@ -1,5 +0,0 @@
-package Labyrinthe;
-
-public class Jeu {
-
-}
---
+++ projet_zeldiablo/src/Labyrinthe/JeuPerso.java
@@ -0,0 +1,18 @@
+package Labyrinthe;
+import moteurJeu.Commande;
+import moteurJeu.Jeu;
+public class JeuPerso implements Jeu{
+
+	@Override
+	public void evoluer(Commande commandeUser) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean etreFini() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
