ea0be6dd5ed694485e7b0114ebc4f4f6b56f2a07
daprile3u <thomas.d-aprile6@etu.univ-lorraine.fr>
2021-06-08 16:45:09 +0200

    Modification effetAjoutMonstre

--- projet_zeldiablo/src/Effet/EffetAjoutMonstre.java
+++ projet_zeldiablo/src/Effet/EffetAjoutMonstre.java
@@ -1,8 +1,10 @@
 package Effet;
 
 import Labyrinthe.Labyrinthe;
+import Personnages.Fantome;
 import Personnages.Heros;
 import Personnages.Monstre;
+import Personnages.Troll;
 
 public class EffetAjoutMonstre implements Effet{
 
@@ -21,11 +23,30 @@ public class EffetAjoutMonstre implements Effet{
 			}
 			nouveauX++;
 		}
+		int r = (int) (Math.random() * 3);
+		Monstre m;
+		switch(r) {
+		case 0:
+			m = new Monstre(15, 22, la, 20);
+			m.setPosX(nouveauX);
+			m.setPosY(nouveauY);
+			la.addMonstre(m);
+			break;
+		case 1:
+			m = new Troll(la);
+			m.setPosX(nouveauX);
+			m.setPosY(nouveauY);
+			la.addMonstre(m);
+			break;
+		case 2:
+			m = new Fantome(la);
+			m.setPosX(nouveauX);
+			m.setPosY(nouveauY);
+			la.addMonstre(m);
+			break;
+			
+		}
 		
-		Monstre m = new Monstre(5, 22, la, 20);
-		m.setPosX(nouveauX);
-		m.setPosY(nouveauY);
-		la.addMonstre(m);
 	}
 
 }
