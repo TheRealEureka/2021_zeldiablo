fd929d3361a4d49db091a16064526003742e8ff1
BullyCimbaluria Kevin <kevin.bully-cimbaluria5@etu.univ-lorraine.fr>
2021-06-08 10:42:58 +0200

    Amélioration de la sandbox

--- projet_zeldiablo/src/Labyrinthe/GenerationLaby.java
+++ projet_zeldiablo/src/Labyrinthe/GenerationLaby.java
@@ -4,44 +4,46 @@ import Personnages.Monstre;
 
 public class GenerationLaby {
 	private Labyrinthe l;
-	public GenerationLaby(Labyrinthe l)
-	{
-		this.l=l;
-	}
-	
-	public void labyTest()
-	{
-		for(int i =0 ; i < 8 ; i++)
-		{
-			l.addCase(new Case(1,i));
-			l.addCase(new Case(i,9));
 
+	public GenerationLaby(Labyrinthe l) {
+		this.l = l;
+	}
+
+	public void labyTest() {
+
+		for (int i = 0; i < 8; i++) {
+			l.addCase(new Case(1, i));
+			for(int y = 9 ; y < 16 ; y++ )
+			{
+			l.addCase(new Case(i, y));
+			}
 		}
-		for(int i =1 ; i < 8 ; i++)
-		{
-			l.addCase(new Case(i,7));		
+
+		for (int y = 7; y >= 0; y--) {
+			for (int i = 1; i <= 15; i++) {
+				l.addCase(new Case(i, y));
+			}
 		}
-		l.addCase(new CaseDeclencheuse(0,1));
+		
+		
+		l.addCase(new CaseDeclencheuse(0, 1));
 		Monstre m = new Monstre();
 		m.setPosXY(0, 3);
 		l.addMonstre(m);
 	}
-	
-	
+
 	public void generationAleatoire() {
 		for (int i = 0; i < 15; i++) {
 			for (int y = 1; y < 15; y++) {
-			int r = (int) (Math.random() * 3);
-			if (r == 1) {
-				l.addCase(new Case(i, y));
-			}else if (r == 2) {
-				
-				l.addCase(new CaseDeclencheuse(i, y));
+				int r = (int) (Math.random() * 3);
+				if (r == 1) {
+					l.addCase(new Case(i, y));
+				} else if (r == 2) {
+
+					l.addCase(new CaseDeclencheuse(i, y));
+				}
 			}
 		}
-	}
-	
-		
-		
+
 	}
 }
--- projet_zeldiablo/src/Personnages/Inventaire.java
+++ projet_zeldiablo/src/Personnages/Inventaire.java
@@ -10,6 +10,20 @@ public class Inventaire {
 		this.tab = new Objet [2];
 	}
 	
+	public Objet getObj(int idx)
+	{
+		Objet o = null;
+		if(idx>=9 && idx<tab.length)
+		{
+			o = tab[idx];
+		}
+		return o;
+	}
+	public int getSize()
+	{
+		return tab.length;
+	}
+	
 	
 
 }
