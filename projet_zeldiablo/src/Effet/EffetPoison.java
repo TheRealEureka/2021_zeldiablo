1cd14c2c3aaecc8d04807399a436caece3451939
Thomas <Thomas@DESKTOP-UAU5GCS>
2021-06-09 22:25:12 +0200

    Wingardium leviosa

--- projet_zeldiablo/src/Effet/EffetPoison.java
+++ projet_zeldiablo/src/Effet/EffetPoison.java
@@ -1,5 +1,7 @@
 package Effet;
 
+
+
 import Personnages.Heros;
 import Personnages.Personnage;
 
@@ -7,15 +9,26 @@ public class EffetPoison implements Effet{
 
 	@Override
 	public void effet(Personnage h) {
-		
-		//h.setSubirPoison(true);
-		//h.subirPoison();
-		long now = System.currentTimeMillis();
-		long freq = 3000;
-		long next = now + freq;
 		int dgt = (int) (h.getVie() * 0.02);
 		int i = 0;
 		
-	}
+		
+		
+
+		
 	
-}
+		while(i < 4){
+			h.subirDegats(dgt);
+			try {
+				Thread.sleep(100);
+				
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+			i++;
+			}
+	
+		}
+	}
+ 	
+
